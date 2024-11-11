af721d1ecd6257300ae47cd67554912a1cf995e1
mlasch01 <mark.lasch@yahoo.com>
2024-11-11 12:54:54 -0600

    11/11/24

---
+++ src/main/java/org/launchcode/techjobsauth/models/dto/RegisterFormDTO.java
@@ -0,0 +1,22 @@
+package org.launchcode.techjobsauth.models.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    @NotNull(message = "Password is required.")
+    @NotBlank(message = "Password is required.")
+    @Size(min = 5, max = 30, message = "Invalid password. Must be between 5 and 30 characters.")
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+}
